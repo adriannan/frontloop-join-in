d1f715e6f0a7ac40663f4ff06f2b205b70d257a3
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-12-09 22:46:02 +0100

    aside-right sect

--- src/layouts/Aside.js
+++ src/layouts/Aside.js
@@ -1,7 +1,66 @@
 import React from "react";
+import img1 from "../images/avatar.jpg";
+import img2 from "../images/avatar2.jpg";
+import img3 from "../images/avatar3.jpg";
 
 const Aside = () => {
-  return <div>Aside</div>;
+  return (
+    <div>
+      <h3 className="aside__tit">people also viewed</h3>
+      <div className="aside__prof">
+        <a href="#">
+          <img src={img1} className="prof__img"></img>
+          <span className="prof__name">Sergey Brin</span>
+          <span className="prof__company">Google</span>
+        </a>
+      </div>
+      <div className="aside__prof">
+        <a href="#">
+          <img src={img2} className="prof__img"></img>
+          <span className="prof__name">Jeffrey Bezos</span>
+          <span className="prof__company">Amazon</span>
+        </a>
+      </div>
+      <div className="aside__prof">
+        <a href="#">
+          <img src={img3} className="prof__img"></img>
+          <span className="prof__name">Steve Jobs</span>
+          <span className="prof__company">Apple</span>
+        </a>
+      </div>
+      <h3 className="aside__tit">more influencers</h3>
+      <div className="aside__infl">
+        <button className="infl__btn-left">
+          {`<`}
+          {/* <i class="material-icons">keyboard_arrow_left</i> */}
+        </button>
+        <a href="#">
+          <img src={img3} className="infl__img"></img>
+          <span className="infl__name">Steve Jobs</span>
+          <span className="infl__company">Apple</span>
+        </a>
+        <button className="infl__btn-right">
+          {`>`}
+          {/* <i class="material-icons">keyboard_arrow_right</i> */}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Aside;
+
+{
+  /* <p className="rec__author">
+  <a href="#">
+    {" "}
+    <img src={img} alt="avatar" className="rec__img"></img>{" "}
+  </a>
+  <h3 className="rec__name">
+    <a href="#">{person}</a>
+  </h3>
+  <span className="rec__company">
+    <a href="#">{company}</a>
+  </span>
+</p>; */
+}
