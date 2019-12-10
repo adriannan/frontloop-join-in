d992b8595bfa629418db9c36c6f2bd245e93cfcd
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-12-10 10:45:48 +0100

    error page

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import "../styles/App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 
 import Footer from "./Footer";
 import Home from "./Home";
+import Error from "./Error";
 
 class App extends Component {
   render() {
@@ -12,10 +13,13 @@ class App extends Component {
       <Router>
         <div className="app">
           <header>{<Header />}</header>
-          <Route
-            path={"/(|background|following|recommendations)/"}
-            component={Home}
-          />
+          <Switch>
+            <Route
+              path={"/(|background|following|recommendations)/"}
+              component={Home}
+            />
+            <Route component={Error} />
+          </Switch>
           <footer>{<Footer />}</footer>
         </div>
       </Router>
---
+++ src/layouts/Error.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Error = () => {
+  return (
+    <section className="error">
+      <h1 className="error__tit">Page not found</h1>
+      <p className="error__txt">
+        Oh, we can’t find the page you’re looking for. Try going back to the
+        previous page or go to our <a href="/">home page</a>
+      </p>
+    </section>
+  );
+};
+
+export default Error;
