dcb29ae371a7a307dc75ff881a7785ea24df83c6
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-12-11 12:20:15 +0100

    connect&follow

--- src/layouts/Error.js
+++ src/layouts/Error.js
@@ -5,7 +5,7 @@ const Error = () => {
       <h1 className="error__tit">Page not found</h1>
       <p className="error__txt">
         Oh, we can’t find the page you’re looking for. Try going back to the
-        previous page or go to our <a href="/">home page</a>
+        previous page or go to our <a href="/">home page</a>.
       </p>
     </section>
   );
--- src/layouts/ProfileInfo.js
+++ src/layouts/ProfileInfo.js
@@ -2,7 +2,10 @@ import React from "react";
 import avatarBG from "../images/bill-gates-avatar.png";
 import "../styles/aside.css";
 
-const ProfileInfo = () => {
+const ProfileInfo = props => {
+  // const handleClick = () => {
+  //   alert("ok");
+  // };
   return (
     <div className="aside__profile">
       <div className="profile__info">
@@ -10,10 +13,27 @@ const ProfileInfo = () => {
         <h3>Bill Gates</h3>
         <h4>Microsoft Founder</h4>
         <p>Greater Seattle Area</p>
-        <button className="btn__follow">Follow</button>
-        <button className="btn__connect">
-          Connect<i class="material-icons">keyboard_arrow_down</i>
-        </button>
+        {props.follow ? (
+          <button className="btn__unfollow" onClick={props.clickUnfollow}>
+            Unfollow
+          </button>
+        ) : (
+          <button className="btn__follow" onClick={props.clickFollow}>
+            Follow
+          </button>
+        )}
+        {props.connect ? (
+          <button
+            className="btn__connect btn__connect-pending"
+            onClick={props.clickConnect}
+          >
+            Pending<i class="material-icons">keyboard_arrow_down</i>
+          </button>
+        ) : (
+          <button className="btn__connect" onClick={props.clickConnect}>
+            Connect<i class="material-icons">keyboard_arrow_down</i>
+          </button>
+        )}
       </div>
       <div className="profile__stats">
         <p>
@@ -41,10 +61,12 @@ const ProfileInfo = () => {
           <a href="https://www.billgates.com.com">billgates.com</a>
         </p>
       </div>
-      <div className="profile__pdf">
-        <i class="material-icons">get_app</i>
-        <p>Export page as PDF</p>
-      </div>
+      <a href="" download="profile" className="profile__pdf-link">
+        <div className="profile__pdf">
+          <i class="material-icons">get_app</i>
+          <p>Export page as PDF</p>
+        </div>
+      </a>
     </div>
   );
 };
