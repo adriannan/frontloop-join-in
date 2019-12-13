557ddd6693c78c5817d974db2c31d970351db9ed
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-12-13 21:51:11 +0100

    login&register validation

--- src/layouts/LoginPage.js
+++ src/layouts/LoginPage.js
@@ -1,32 +1,124 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../images/logo.png";
 import "../styles/login.css";
 
-const LoginPage = props => {
-  return (
-    <section className="content__login">
-      <h3 className="login__logo">
-        Join<img src={logo}></img>
-      </h3>
-      <h2 className="login__tit">Welcome Back</h2>
-      <p className="login__subtit">
-        Don't miss your next opportunity. Sign in to stay updated on your
-        professional world.
-      </p>
-      <form className="login__form">
-        <label>
-          Email or Phone: <input type="text"></input>
-        </label>
-        <label>
-          Password: <input type="password"></input>
-        </label>
-        <button className="login__btn">Sign in</button>
-      </form>
-      <p className="form__footer">
-        New to JoinIn? <a href="/register">Join now</a>
-      </p>
-    </section>
-  );
-};
+class LoginPage extends Component {
+  state = {
+    username: "",
+    password: "",
+    errors: {
+      username: false,
+      username_length: false,
+      password: false,
+      password_length: false
+    }
+  };
+  alerts = {
+    username_alert: "Incorrect email, phone number or password",
+    username_length_alert: "Field is required",
+    password_alert: "Incorrect password",
+    password_length_alert: "Password is required"
+  };
+  handleChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const validation = this.formValidation();
+
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        password: ""
+      });
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          username_length: !validation.username_length,
+          password: !validation.password,
+          password_length: !validation.password_length
+        }
+      });
+    }
+  };
+  formValidation = () => {
+    let username = false,
+      username_length = false,
+      password = false,
+      password_length = false,
+      correct = false;
+    if (this.state.username.length !== 0) {
+      username_length = true;
+    }
+    if (this.state.username == 0) {
+      username = true;
+    }
+    if (this.state.password.length !== 0) {
+      password_length = true;
+    }
+    if (this.state.password == 0) {
+      password = true;
+    }
+    if (username && username_length && password && password_length) {
+      correct = true;
+    }
+    return { username, username_length, password, password_length, correct };
+  };
+  render() {
+    return (
+      <section className="content__login">
+        <h3 className="login__logo">
+          Join<img src={logo}></img>
+        </h3>
+        <h2 className="login__tit">Welcome Back</h2>
+        <p className="login__subtit">
+          Don't miss your next opportunity. Sign in to stay updated on your
+          professional world.
+        </p>
+        <form className="login__form" onSubmit={this.handleSubmit}>
+          <label>
+            Email or Phone:{" "}
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            ></input>
+            {this.state.errors.username && (
+              <p className="form-error">{this.alerts.username_alert}</p>
+            )}
+            {this.state.errors.username_length && (
+              <p className="form-error">{this.alerts.username_length_alert}</p>
+            )}
+          </label>
+          <label>
+            Password:{" "}
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            ></input>
+            {this.state.errors.password && (
+              <p className="form-error">{this.alerts.password_alert}</p>
+            )}
+            {this.state.errors.password_length && (
+              <p className="form-error">{this.alerts.password_length_alert}</p>
+            )}
+          </label>
+          <button className="login__btn">Sign in</button>
+        </form>
+        <p className="form__footer">
+          New to JoinIn? <a href="/register">Join now</a>
+        </p>
+      </section>
+    );
+  }
+}
 
 export default LoginPage;
--- src/layouts/RegisterPage.js
+++ src/layouts/RegisterPage.js
@@ -1,42 +1,236 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../images/logo.png";
 import "../styles/login.css";
 
-const RegisterPage = props => {
-  return (
-    <section className="content__register">
-      <h3 className="register__logo">
-        Join<img src={logo}></img>
-      </h3>
-      <h2 className="register__tit">Make the most of your professional life</h2>
-      <form className="register__form">
-        <label>
-          First name: <input type="text"></input>
-          {/* <p className="form-error">Niepoprawna nazwa uytkownika</p> */}
-        </label>
-        <label>
-          Last name: <input type="text"></input>
-        </label>
-        <label>
-          Email: <input type="mail"></input>
-        </label>
-        <label>
-          Password: <input type="password"></input>
-        </label>
-        <label>
-          Confirm password: <input type="password"></input>
-        </label>
-        <p className="form__privacy">
-          You agree to the LinkedIn User Agreement, Privacy Policy, and Cookie
-          Policy.
+class RegisterPage extends Component {
+  state = {
+    username: "",
+    userLastname: "",
+    email: "",
+    password: "",
+    passwordConf: "",
+    correct: false,
+    errors: {
+      username: false,
+      userLastname: false,
+      email: false,
+      password: false,
+      password_length: false,
+      passwordConf: false
+    }
+  };
+  alerts = {
+    username_alert: "Field is required",
+    userLastname_alert: "Field is required",
+    email_alert: "Please, enter a valid email address",
+    password_alert: "Password is required",
+    password_length_alert: "Password is too short (min. 8 characters)",
+    // passwordConf_alert: "Confirmation failed",
+    passwordConf_alert: "Confirmation doesn't match"
+  };
+
+  handleChange = e => {
+    // console.log(e.target.name);
+    // console.log(e.target.type);
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const validation = this.formValidation();
+
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        userLastname: "",
+        email: "",
+        password: "",
+        passwordConf: "",
+        correct: true,
+        errors: {
+          username: false,
+          userLastname: false,
+          email: false,
+          password: false,
+          password_length: false,
+          passwordConf: false
+        }
+      });
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          userLastname: !validation.userLastname,
+          email: !validation.email,
+          password: !validation.password,
+          password_length: !validation.password_length,
+          passwordConf: !validation.passwordConf
+        }
+      });
+    }
+  };
+  formValidation = () => {
+    let username = false,
+      userLastname = false,
+      email = false,
+      password = false,
+      password_length = false,
+      passwordConf = false,
+      correct = false;
+    if (
+      this.state.username.length > 0 &&
+      this.state.username.indexOf(" ") == -1
+    ) {
+      username = true;
+    }
+    if (
+      this.state.userLastname.length > 0 &&
+      this.state.userLastname.indexOf(" ") == -1
+    ) {
+      userLastname = true;
+    }
+    if (
+      this.state.email.indexOf("@") !== -1 &&
+      this.state.email.indexOf(".") !== -1
+    ) {
+      email = true;
+    }
+    if (this.state.password.length !== 0) {
+      password = true;
+    }
+    if (this.state.password.length >= 8 || this.state.password.length == 0) {
+      password_length = true;
+    }
+    if (this.state.password === this.state.passwordConf) {
+      passwordConf = true;
+    }
+    if (
+      username &&
+      userLastname &&
+      email &&
+      password &&
+      password_length &&
+      passwordConf
+    ) {
+      correct = true;
+    }
+    return {
+      correct,
+      username,
+      userLastname,
+      email,
+      password,
+      password_length,
+      passwordConf
+    };
+  };
+  componentDidUpdate() {
+    if (this.state.correct) {
+      setTimeout(() => {
+        this.setState({
+          correct: false
+        });
+      }, 5000);
+    }
+  }
+
+  render() {
+    return (
+      <section className="content__register">
+        <h3 className="register__logo">
+          Join<img src={logo}></img>
+        </h3>
+        <h2 className="register__tit">
+          Make the most of your professional life
+        </h2>
+        <form className="register__form" onSubmit={this.handleSubmit}>
+          <label>
+            First name:{" "}
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            ></input>
+            {this.state.errors.username && (
+              <p className="form-error">{this.alerts.username_alert}</p>
+            )}
+          </label>
+          <label>
+            Last name:{" "}
+            <input
+              type="text"
+              name="userLastname"
+              value={this.state.userLastname}
+              onChange={this.handleChange}
+            ></input>
+            {this.state.errors.userLastname && (
+              <p className="form-error">{this.alerts.userLastname_alert}</p>
+            )}
+          </label>
+          <label>
+            Email:{" "}
+            <input
+              type="mail"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            ></input>
+            {this.state.errors.email && (
+              <p className="form-error">{this.alerts.email_alert}</p>
+            )}
+          </label>
+          <label>
+            Password:{" "}
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            ></input>
+            {this.state.errors.password && (
+              <p className="form-error">{this.alerts.password_alert}</p>
+            )}
+            {this.state.errors.password_length && (
+              <p className="form-error">{this.alerts.password_length_alert}</p>
+            )}
+          </label>
+          <label>
+            Confirm password:{" "}
+            <input
+              type="password"
+              name="passwordConf"
+              value={this.state.passwordConf}
+              onChange={this.handleChange}
+            ></input>
+            {this.state.errors.passwordConf && (
+              <p className="form-error">{this.alerts.passwordConf_alert}</p>
+            )}
+          </label>
+          {this.state.correct ? (
+            <h2 style={{ textAlign: "center", color: "#09589e" }}>
+              Welcome to JoinIn!
+            </h2>
+          ) : (
+            <>
+              {" "}
+              <p className="form__privacy">
+                You agree to the LinkedIn User Agreement, Privacy Policy, and
+                Cookie Policy.
+              </p>
+              <button className="register__btn">Agree &amp; Join</button>
+            </>
+          )}
+        </form>
+        <p className="form__footer">
+          Already on JoinIn? <a href="/login">Sing in</a>
         </p>
-        <button className="register__btn">Agree &amp; Join</button>
-      </form>
-      <p className="form__footer">
-        Already on JoinIn? <a href="/login">Sing in</a>
-      </p>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default RegisterPage;
