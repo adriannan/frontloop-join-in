f53fc5c319c1e861e54f7199ea840b417d5c48e3
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-12-15 23:40:05 +0100

    Updates

---
+++ precache-manifest.39e92db417649b137e70bf5fd14495cd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e4078155e4e455b8d2314f36bf36f1",
+    "url": "/frontloop-join-in/index.html"
+  },
+  {
+    "revision": "59a0a29f0eef36c40039",
+    "url": "/frontloop-join-in/static/css/main.ca106bf3.chunk.css"
+  },
+  {
+    "revision": "89052b5b79b38644115b",
+    "url": "/frontloop-join-in/static/js/2.3f0aa6af.chunk.js"
+  },
+  {
+    "revision": "59a0a29f0eef36c40039",
+    "url": "/frontloop-join-in/static/js/main.14da4d21.chunk.js"
+  },
+  {
+    "revision": "09e8d6d9c15975b734d7",
+    "url": "/frontloop-join-in/static/js/runtime-main.c6762410.js"
+  },
+  {
+    "revision": "39c7a8a4d522df20e624ade33d38fb69",
+    "url": "/frontloop-join-in/static/media/article1.39c7a8a4.jpg"
+  },
+  {
+    "revision": "58d7ed06df1c1b0dcd5f799ff0cb3566",
+    "url": "/frontloop-join-in/static/media/article2.58d7ed06.jpg"
+  },
+  {
+    "revision": "790774d8de37efd9e20781798079ce63",
+    "url": "/frontloop-join-in/static/media/article3.790774d8.jpg"
+  },
+  {
+    "revision": "63681f0bbfdbbdba66b8d8cbe95c6771",
+    "url": "/frontloop-join-in/static/media/bill-gates-avatar.63681f0b.png"
+  },
+  {
+    "revision": "2f19b9c9d492f8107fe26bd31bdfda5f",
+    "url": "/frontloop-join-in/static/media/logo-amazon.2f19b9c9.jpg"
+  },
+  {
+    "revision": "a4187bc04c137613fecea265611f65a4",
+    "url": "/frontloop-join-in/static/media/logo-microsoft.a4187bc0.png"
+  },
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "/frontloop-join-in/static/media/logo-youtube.07b8560a.png"
+  },
+  {
+    "revision": "76cb51edd4f845b69c1b9b04f31fef32",
+    "url": "/frontloop-join-in/static/media/lost.76cb51ed.jpg"
+  }
+]);
